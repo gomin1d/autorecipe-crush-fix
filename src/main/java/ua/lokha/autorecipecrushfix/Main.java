ee8bac7d67c36714b82605340565302f37ba4fb7
lokha <aleksey.kovalchuk98@gmail.com>
2021-04-10 04:24:47 +0300

    готово

--- src/main/java/ua/lokha/autorecipecrushfix/Main.java
+++ src/main/java/ua/lokha/autorecipecrushfix/Main.java
@@ -1,7 +1,70 @@
 package ua.lokha.autorecipecrushfix;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Main extends JavaPlugin implements Listener {
+    private Map<String, Limit> lastAutoRecipe = new ConcurrentHashMap<>();
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(
+                this, PacketType.Play.Client.AUTO_RECIPE
+        ) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                String playerName = event.getPlayer().getName();
+                Limit limit = lastAutoRecipe.computeIfAbsent(playerName, s -> new Limit());
+
+                if (limit.checkLimit()) {
+                    Main.this.getLogger().warning("Игрок " + playerName + " " +
+                            "слишком часто отправляет AutoRecipe пакеты, отменяем пакет." );
+                    event.setCancelled(true);
+                }
+            }
+        });
+    }
+
+    @EventHandler
+    public void on(PlayerQuitEvent event) {
+        lastAutoRecipe.remove(event.getPlayer().getName());
+    }
+
+    /**
+     * Token Bucket Algorithm
+     */
+    public static class Limit {
+        private int limit = 16;
+        private long lastCheck = System.currentTimeMillis();
+
+        public boolean checkLimit() {
+            long current = System.currentTimeMillis();
+
+            long left = current - lastCheck;
+            int addLimit = (int) (left / 500);
+            if (addLimit > 0) {
+                this.limit = Math.min(16, this.limit + addLimit);
+                this.lastCheck = current;
+            }
+
+            if (this.limit <= 0) {
+                return true;
+            } else {
+                this.limit--;
+                return false;
+            }
+        }
+    }
 }
